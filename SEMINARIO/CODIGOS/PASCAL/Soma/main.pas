39fac17e0ba79bd2b5e27d394fbb684ce8f5236f
eduhatcs <eduardolwk@gmail.com>
2025-11-18 16:10:40 -0300

    implementação do laço while

--- SEMINARIO/CODIGOS/PASCAL/Soma/main.pas
+++ SEMINARIO/CODIGOS/PASCAL/Soma/main.pas
@@ -7,14 +7,30 @@ var
 	resultado: Integer;
 	a: Integer;
 	b: Integer;
+	i: Integer;
+	
 begin
-	writeln('Soma de dois valores');
-	write('Digite o valor de A: ');
-	readln(a);
+	writeln('Bem vindo(a) ao programa de soma de dois valores!');
+	writeln('[0] - Sair');
+	writeln('[1] - Continuar');
+	write('Insira sua opção: ');
+	readln(i);
+	
+	while i <> 0 do
+	begin
+		write('Digite o valor de A: ');
+		readln(a);
 
-	write('Digite o valot de B: ');
-	readln(b);
+		write('Digite o valot de B: ');
+		readln(b);
 
-	resultado := soma(a, b);
-	writeln('A soma final é: ', resultado);
+		resultado := soma(a, b);
+		writeln('A soma final é: ', resultado);
+
+		writeln();
+		writeln('[0] - Sair');
+		writeln('[1] - Continuar');
+		write('Insira sua opção: ');
+		readln(i);
+	end;
 end.
